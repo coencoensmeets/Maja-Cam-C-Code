0d2e25486d2d2519fd58876308b4e5aed0701c4d
coencoensmeets <coencoensmeets@gmail.com>
2025-11-27 22:01:20 +0100

    Release v1.0.0-test.1

--- main/ota_manager.h
+++ main/ota_manager.h
@@ -83,7 +83,7 @@ void ota_manager_destroy(OTAManager_t *manager);
 #define GITHUB_RAW_CONTENT "https://raw.githubusercontent.com/%s/%s/%s/%s"
 
 // Firmware version - should match your build version
-#define FIRMWARE_VERSION "testing-test.1"
+#define FIRMWARE_VERSION "v1.0.0-test.1"
 
 // OTA buffer size
 #define OTA_BUFFER_SIZE 1024
